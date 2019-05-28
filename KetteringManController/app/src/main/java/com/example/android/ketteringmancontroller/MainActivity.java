96c754f7127443e38010ecd253af2b391810ab4f
Logan Thomas <ltogan.2014@gmail.com>
2019-05-28 15:40:50 -0400

    Add leaderboard activity and play activity to KetteringManController phone app project

--- KetteringManController/app/src/main/java/com/example/android/ketteringmancontroller/MainActivity.java
+++ KetteringManController/app/src/main/java/com/example/android/ketteringmancontroller/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.android.ketteringmancontroller;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mPlayButton;
+    Button mLeaderboardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlayButton = findViewById(R.id.play_button);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mLeaderboardButton = findViewById(R.id.leaderboard_button);
+        mLeaderboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LeaderboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
